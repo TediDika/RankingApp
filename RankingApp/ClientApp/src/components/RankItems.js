1cb74c752892712d89332771b1f4ecdb2b09520d
TediDika <txd190008@utdallas.edu>
2023-09-13 20:08:47 -0500

    Added MovieImages component

--- RankingApp/ClientApp/src/components/RankItems.js
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -1,4 +1,5 @@
 ﻿import React, { useState, useEffect } from 'react';
+import MovieImageArr from "./MovieImages.js";
 
 const RankItems = () => {
 
@@ -17,11 +18,17 @@ const RankItems = () => {
     },[])
 
     return (
-        <main> 
+        <main>
+            <div className = "items-not-ranked">
             {
-                (items.length > 0) ? items.map((item) => <h3>{item.title }</h3>):<div>Loading...</div>
+                    (items.length > 0) ? items.map((item) =>
+                    <div className = "unranked-cell">
+                        <img id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)?.image } />
+                    </div>
+                    ) : <div>Loading...</div>
+                    
             }
-           
+            </div>
         </main>
     )
 }
