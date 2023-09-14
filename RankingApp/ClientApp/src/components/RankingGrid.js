da89015d9338ba5571a56b9b8f6aa211f37d2e49
TediDika <txd190008@utdallas.edu>
2023-09-13 20:30:16 -0500

    Completed RankingGrid methods

--- RankingApp/ClientApp/src/components/RankingGrid.js
+++ RankingApp/ClientApp/src/components/RankingGrid.js
@@ -1,4 +1,4 @@
-﻿const RankingGrid = ({ items, imgArr }) => {
+﻿const RankingGrid = ({ items, imgArr, drag, allowDrop, drop }) => {
 
     const rankingGrid = [];
     const cellCollectionTop = [];
@@ -6,7 +6,52 @@
     const cellCollectionBottom = [];
     const cellCollectionWorst = [];
 
-    
+    function pushCellMarkupToArr(cellCollection, rankNum, rowLabel) {
+        if (rankNum > 0) {
+            var item = items.find(o => o.ranking === rankNum);
+            cellCollection.push(<div id={`rank-${rankNum}`} onDrop={drop} onDragOver={allowDrop} className="rank-cell">
+                {(item != null) ? <img id={`item-${item.id}`} src={imgArr.find(o => o.id === item.imageId)?.image} draggable="true" onDragStart={drag} />
+                    : null}
+            </div>);
+        }
+        else {
+            cellCollection.push(<div className="row-label">
+                <h4>{rowLabel}</h4>
+            </div>);
+        }
+    }
+
+    function createCellsForRows(rowNum) {
+        var rankNum = 0;
+        var currCollection = [];
+        var label = "";
+        const numCells = 5;
+
+        for (var a = 1; a <= numCells; a++) {
+            rankNum = (a === 1) ? 0 : (numCells * (rowNum - 1)) + a - rowNum;
+
+            if (rowNum === 1) {
+                currCollection = cellCollectionTop;
+                label = "Top Tier";
+            }
+            else if (rowNum === 2) {
+                currCollection = cellCollectionMiddle;
+                label = "Middle Tier";
+            }
+            else if (rowNum === 3) {
+                currCollection = cellCollectionBottom;
+                label = "Bottom Tier";
+            }
+            else if (rowNum === 4) {
+                currCollection = cellCollectionWorst;
+                label = "Worst Tier";
+            }
+            pushCellMarkupToArr(currCollection, rankNum, label);
+
+        }
+
+    }
+
 
     function createCellForRows() {
 
